fe1cce912da4de328d3f6cb8c1f2b9eac0b30395
Dor Weitman <dorweitman@gmail.com>
2020-09-30 00:59:49 +0300

    fix: timer default time

--- src/components/event/config/run.js
+++ src/components/event/config/run.js
@@ -1,7 +1,11 @@
+const moment = require('moment');
+
+const defaultTime = moment().startOf('day');
+
 const endTime = {
     name: 'end_time',
     translation: 'זמן סיום',
-    default: 0,
+    default: defaultTime,
     type: 'timer',
     min: 0,
     max: 59,
@@ -9,7 +13,7 @@ const endTime = {
 const startTime = {
     name: 'start_time',
     translation: 'זמן התחלה',
-    default: 0,
+    default: defaultTime,
     type: 'timer',
     min: 0,
     max: 59,
